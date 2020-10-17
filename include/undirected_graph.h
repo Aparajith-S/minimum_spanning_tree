6c04f60324b8696b658e836553073398759f2bb8
Aparajith <s.aparajith@live.com>
2020-10-18 01:37:10 +0200

    refactor graph class for Kruskals MST implementation

--- include/undirected_graph.h
+++ include/undirected_graph.h
@@ -17,141 +17,121 @@
 #include<unordered_map>
 #include<float.h>
 #include<fstream>
+#include"priority_queue.h"
 #include"types.h"
 
-///////////////////////////////////////
-// function prototypes
-///////////////////////////////////////
+namespace undirectedGraph {
+	///////////////////////////////////////
+	// function prototypes
+	///////////////////////////////////////
 
-/// @brief : vertex name generator
-/// @details : generates increasing one or two letter names based on vertex index.
-///            sort of how an MS excel table columns increase A,...,Z,AA,...AZ,... so on till ZZ.
-///            which is 702 vertices maximum this name generator can handle.
-/// @returns : string containing the generated vertex name.
-std::string name_generator(uint32 vertex_idx);
+	/// @brief : vertex name generator
+	/// @details : generates increasing one or two letter names based on vertex index.
+	///            sort of how an MS excel table columns increase A,...,Z,AA,...AZ,... so on till ZZ.
+	///            which is 702 vertices maximum this name generator can handle.
+	/// @returns : string containing the generated vertex name.
+	std::string name_generator(uint32 vertex_idx);
 
-///////////////////////////////////////
-// Class definitions
-///////////////////////////////////////
-
-/// @class Graph Storage
-/// @details creates and manages a graph based on adjacency lists
-class Graph
-{
-	//fwd declarations 
-	struct Edge;
-	struct Vertex;
-public:
-
-	///@brief constructor that takes number of vertices and creates a graph with only vertices and no edges.
-	///@see DijkstraSimulation class in DijkstraSimulaiton.h to understand how a random graph is created. 
-	Graph(const uint32 NrOfVertices) : vertices() 
-	{ 
-		Vertex temp = Vertex();
-		vertices.rehash(NrOfVertices);
-		std::string name;
-		for (uint32 i = 0; i < NrOfVertices; i++)
-		{ 
-			name = name_generator(i);
-			vertices[name]=temp;
-		}
-	};
-
-	///@brief constructor that takes number of vertices and creates a graph with vertices and egdes as it is read from a file
-	///@see ReadMe.md to understand how the file needs to be input
-	Graph(const std::string& fileName);
-
-	virtual ~Graph() 
-	{
-		//empty destructor as vector is responsible to free the memory it allocates during destruction
-	}
-
-	///@brief fetches the number of vertices in the graph
-	///@return unsigned integer number of vertices
-	uint32 V();
-	
-	///@brief fetches the number of edges in the graph
-	///@return unsigned integer number of edges
-	uint32 E();
-
-	///@brief fetches a list of neighbors of the node name in the argument
-	///@param[in] x : node name 
-	///@return list of neighboring node names
-	std::vector<std::string> neighbors(const std::string& x);
-
-	///@brief tests if the given nodes are neigbors or not
-	///@param[in] x: node name string
-	///@param[in] y: node name string
-	///@return true if they have an edge
-	///@return false if they don-t have an edge
-	bool adjacent(const std::string& x, const std::string& y);
-
-	///@brief adds an edge between the two nodes and assigns the distance 
-	
-	///@param[in] edgeValue : distance between x and y
-	///@return true if the operation was successful
-	///@return false if the operation fails
-	bool add(const std::string& x, const std::string& y, const float edgeValue);
-
-	///@brief removes an edge between the two nodes
-	///@param[in] x: node name string
-	///@param[in] y: node name string
-	///@return true if the operation was successful
-	///@return false if the operation fails
-	bool del(const std::string& x, const std::string& y);
-
-	///@brief get the nodevalue
-	///@param[in] x: node name string
-	///@return cost associated with the node
-	float getNodeValue(const std::string& x);
-
-
-	///@brief get the nodevalue
-	///@param[in] x: node name string
-	///@param[in] nodeValue: node value
-	///@return true if the operation was successful
-	///@return false if the operation fails
-	bool setNodeValue(const std::string& x, const float nodeValue);
-
-	///@brief get the edge value or distance between the two nodes
-	///@details constant time to identify the vertex and O(E) to access the edge
-	///@param[in] x: node name string
-	///@param[in] y: node name string
-	///@return distance between x and y
-	float getEdgeValue(const std::string& x, const std::string& y);
-
-	///@brief sets the EdgeValue or the distance between two nodes
-	///@details constant time to identify the vertex and constant time to push the edge
-	///@param[in] x: node name string
-	///@param[in] y: node name string
-	///@param[in] edgeValue : distance between x and y
-	///@return true if the operation was successful
-	///@return false if the operation fails
-	bool setEdgeValue(const std::string& x, const std::string& y, const float edgeValue);
-
-private:
+	///////////////////////////////////////
+	// Class definitions
+	///////////////////////////////////////
 
 	struct Edge
 	{
-		std::string node; // node to which it is an edge to
+		std::string start_node; // node from which it is an edge
+		std::string end_node; // node to which it is an edge
 		float value; // weight of the edge such as distance
 		Edge() {}
-		Edge(const std::string& node, const float& val):node(node),value(val){}
-		virtual ~Edge(){}
+		Edge(const std::string& src_node,
+			const std::string& dst_node,
+			const float& val) :start_node(src_node), end_node(dst_node), value(val) {}
+		virtual ~Edge() {}
 	};
-	
-	struct Vertex
+
+	/// @class Graph Storage
+	/// @details creates and manages a graph based on adjacency lists
+	class Graph
 	{
-		float value; // distance from the source
-		std::vector<Edge> edges; 
+	public:
 
-		/// @brief constructor
-		/// @details set initial value to infinity i.e. max Float value
-		Vertex():value(FLT_MAX), edges(){}
-		Vertex(const float& val) :value(val),edges(){}
-		virtual ~Vertex() {}
+		Graph(const uint32 nodes);
+
+		///@brief constructor that takes number of vertices and creates a graph with vertices and egdes as it is read from a file
+		///@see ReadMe.md to understand how the file needs to be input
+		Graph(const std::string& fileName);
+
+		virtual ~Graph()
+		{
+			//empty destructor as vector is responsible to free the memory it allocates during destruction
+		}
+
+		///@brief fetches the number of vertices in the graph
+		///@return unsigned integer number of vertices
+		uint32 V();
+
+		///@brief fetches the number of edges in the graph
+		///@return unsigned integer number of edges
+		uint32 E();
+
+		///@brief fetches a list of neighbors of the node name in the argument
+		///@param[in] x : node name 
+		///@return list of neighboring node names
+		std::vector<std::string> neighbors(const std::string& x);
+
+		///@brief tests if the given nodes are neigbors or not
+		///@param[in] x: node name string
+		///@param[in] y: node name string
+		///@return true if they have an edge
+		///@return false if they don-t have an edge
+		bool adjacent(const std::string& x, const std::string& y);
+
+		///@brief adds an edge between the two nodes and assigns the distance 
+		///@param[in] edgeValue : distance between x and y
+		///@return true if the operation was successful
+		///@return false if the operation fails
+		bool add(const std::string& x, const std::string& y, const float edgeValue);
+
+		///@brief removes an edge between the two nodes
+		///@param[in] x: node name string
+		///@param[in] y: node name string
+		///@return true if the operation was successful
+		///@return false if the operation fails
+		bool del(const std::string& x, const std::string& y);
+
+		///@brief get the nodevalue
+		///@param[in] x: node name string
+		///@return cost associated with the node
+		float getNodeValue(const std::string& x);
+
+		///@brief get the nodevalue
+		///@param[in] x: node name string
+		///@param[in] nodeValue: node value
+		///@return true if the operation was successful
+		///@return false if the operation fails
+		bool setNodeValue(const std::string& x, const float nodeValue);
+
+		///@brief get the edge value or distance between the two nodes
+		///@details constant time to identify the vertex and O(E) to access the edge
+		///@param[in] x: node name string
+		///@param[in] y: node name string
+		///@return distance between x and y
+		float getEdgeValue(const std::string& x, const std::string& y);
+
+		///@brief sets the EdgeValue or the distance between two nodes
+		///@details constant time to identify the vertex and constant time to push the edge
+		///@param[in] x: node name string
+		///@param[in] y: node name string
+		///@param[in] edgeValue : distance between x and y
+		///@return true if the operation was successful
+		///@return false if the operation fails
+		bool setEdgeValue(const std::string& x, const std::string& y, const float edgeValue);
+
+		std::vector<Edge> getEdgeList();
+
+	private:
+
+		uint32 numNodes;
+		std::vector<Edge> edgeList; ///< store Edge list and its properties
+		std::vector<std::pair<std::string, float>> nodeCost;
 	};
-
-	std::unordered_map<std::string,Vertex> vertices; ///< store name of vertex and its properties
-};
-
+}
--- source/undirected_graph.cpp
+++ source/undirected_graph.cpp
@@ -7,228 +7,203 @@
 
 #include"undirected_graph.h"
 #include<algorithm>
-
-/// @brief : vertex name generator
-/// @see : undirected_graph.h for more details
-std::string name_generator(uint32 vertex_idx) 
-{
-	std::string temp;
-	int number = vertex_idx;
-	int i = 0;
-	if (number > 25)
+#include"myalgorithm.h"
+namespace undirectedGraph {
+	/// @brief : vertex name generator
+	/// @see : undirected_graph.h for more details
+	std::string name_generator(uint32 vertex_idx)
 	{
-		int first = number / 26;
-		temp.push_back(char(64 + first));
-		int second = number % 26;
-		temp.push_back(char(65 + second));
-	}
-	else
-	{
-		int second = number % 26;
-		temp.push_back(char(65 + second));
-	}
-	return temp;
-}
-
-Graph::Graph(const std::string& fileName) : vertices()
-{
-	Vertex temp = Vertex();
-	std::ifstream fptr(fileName);
-	std::istream_iterator<std::string> fstart(fptr);
-	std::istream_iterator<std::string> fend;
-	uint16 NrOfVertices = 0;
-	std::vector<std::string> words(fstart,fend);
-	if (words.size())
-	{
-		NrOfVertices = std::stoi(words.at(0));
-		vertices.rehash(NrOfVertices);
-		std::string name;
-		for (uint32 i = 0; i < NrOfVertices; i++)
+		std::string temp;
+		int number = vertex_idx;
+		int i = 0;
+		if (number > 25)
 		{
-			name = name_generator(i);
-			vertices[name] = temp;
+			int first = number / 26;
+			temp.push_back(char(64 + first));
+			int second = number % 26;
+			temp.push_back(char(65 + second));
+		}
+		else
+		{
+			int second = number % 26;
+			temp.push_back(char(65 + second));
+		}
+		return temp;
+	}
+	Graph::Graph(const uint32 nodes) :numNodes(nodes)
+	{}
+
+	Graph::Graph(const std::string& fileName) : numNodes(0), edgeList()
+	{
+		std::ifstream fptr(fileName);
+		std::istream_iterator<std::string> fstart(fptr);
+		std::istream_iterator<std::string> fend;
+		uint16 NrOfVertices = 0;
+		std::vector<std::string> words(fstart, fend);
+		if (words.size())
+		{
+			NrOfVertices = std::stoi(words.at(0));
+		}
+		if (NrOfVertices)
+		{
+			numNodes = NrOfVertices;
+			for (auto iter = words.begin() + 1;
+				iter != words.end();
+				)
+			{
+				//first is the source vertex
+				uint16 src = std::stoi(*iter);
+				iter++;
+				//second is the destination vertex
+				uint16 dest = std::stoi(*iter);
+				iter++;
+				//third is the edge value
+				float32 edge = std::stof(*iter);
+				iter++;
+				this->add(name_generator(src), name_generator(dest), edge);
+			}
 		}
 	}
-	if (NrOfVertices)
+
+	uint32 Graph::V()
 	{
-		for (auto iter = words.begin() + 1;
-			iter != words.end();
-			)
-		{
-			//first is the source vertex
-			uint16 src = std::stoi(*iter);
-			iter++;
-			//second is the destination vertex
-			uint16 dest = std::stoi(*iter);
-			iter++;
-			//third is the edge value
-			float32 edge = std::stof(*iter);
-			iter++;
-			this->add(name_generator(src), name_generator(dest), edge);
-		}
+		return numNodes;
 	}
-}
 
-uint32 Graph::V() 
-{
-	return vertices.size();
-}
-
-uint32 Graph::E()
-{
-	uint32 sum = 0;
-	for (auto iter = vertices.begin(); 
-		iter != vertices.end(); 
-		iter++)
-	{	
-		sum+= iter->second.edges.size();
-	}
-	return sum;
-}
-
-std::vector<std::string> Graph::neighbors(const std::string& x) 
-{
-	std::vector<std::string> temp= std::vector<std::string>();
-	if (vertices.find(x)!= vertices.end())
+	uint32 Graph::E()
 	{
-		for (auto iter = vertices.at(x).edges.begin(); 
-			iter != vertices.at(x).edges.end(); 
+		return edgeList.size() / 2;
+	}
+
+	std::vector<std::string> Graph::neighbors(const std::string& x)
+	{
+		std::vector<std::string> temp;
+		for (auto iter = edgeList.begin();
+			iter != edgeList.end();
 			iter++)
 		{
-			temp.push_back(iter->node);
+			if (iter->start_node == x)
+			{
+				temp.push_back(iter->end_node);
+			}
 		}
+		return temp;
 	}
-	return temp;
-}
 
-bool Graph::adjacent(const std::string& x, const std::string& y)
-{
-	bool isTrue = false;
-    auto found = std::find_if(vertices.at(x).edges.begin(), 
-		vertices.at(x).edges.end(), 
-		[&](Edge edge) {return (edge.node == y);});
-	
-	isTrue = found != vertices.at(x).edges.end();
-	return isTrue;
-}
-
-bool Graph::add(const std::string& x, const std::string& y, const float edgeValue)
-{
-	bool success=false;
-	if (vertices.find(x) != vertices.end() && vertices.find(y) != vertices.end())
+	bool Graph::adjacent(const std::string& x, const std::string& y)
 	{
-		success = true;
-		Edge temp(y, edgeValue);
-		vertices.at(x).edges.push_back(temp);
-		temp.node = x;
-		vertices.at(y).edges.push_back(temp);
+		bool isTrue = false;
+		auto found = std::find_if(edgeList.begin(),
+			edgeList.end(),
+			[&](Edge edge) {return (edge.start_node == x && edge.end_node == y); });
+		isTrue = found != edgeList.end();
+		return isTrue;
 	}
-	return success;
-}
 
-bool Graph::del(const std::string& x, const std::string& y) 
-{
-
-	bool success = false;
-	if (vertices.find(x) != vertices.end() && vertices.find(y) != vertices.end())
+	bool Graph::add(const std::string& x, const std::string& y, const float edgeValue)
 	{
+		bool success = false;
+		Edge temp(x, y, edgeValue);
+		if (edgeList.size())
+		{
+			auto pos = myalgo::binary_search(edgeList.begin(),
+				edgeList.end(),
+				temp,
+				[&](Edge left, Edge right) {return (left.value > right.value); });
+
+			static_cast<void>(edgeList.insert(pos, temp));
+			success = true;
+		}
+		else
+		{
+			success = true;
+			edgeList.push_back(temp);
+		}
+		return success;
+	}
+
+	bool Graph::del(const std::string& x, const std::string& y)
+	{
+
+		bool success = false;
 		success = adjacent(x, y);
 		if (success)
 		{
-
-			for (auto iter = vertices.at(x).edges.begin();
-				iter != vertices.at(x).edges.end();
+			uint8 count = 0;
+			for (auto iter = edgeList.begin();
+				iter != edgeList.end();
 				iter++)
 			{
-				if (iter->node == y)
+				if (iter->start_node == x && iter->end_node == y ||
+					iter->start_node == y && iter->end_node == x)
 				{
-					vertices.at(x).edges.erase(iter);
-					break;
+					success = true;
+					edgeList.erase(iter);
+					if (++count == 2)
+					{
+						break;
+					}
 				}
 			}
 
-			for (auto iter = vertices.at(y).edges.begin();
-				iter != vertices.at(y).edges.end();
-				iter++)
-			{
-				if (iter->node == x)
-				{
-					vertices.at(y).edges.erase(iter);
-					break;
-				}
-			}
+		}
+		return success;
+	}
 
-
+	float Graph::getNodeValue(const std::string& x)
+	{
+		auto found = find_if(nodeCost.begin(),
+			nodeCost.end(),
+			[&](std::pair<std::string, float> val) {return val.first == x; });
+		if (found != nodeCost.end())
+		{
+			return found->second;
+		}
+		else
+		{
+			return FLT_MAX;
 		}
 	}
-	return success;
-}
 
-float Graph::getNodeValue(const std::string& x)
-{
-	if (vertices.find(x) != vertices.end())
+	bool Graph::setNodeValue(const std::string& x, const float nodeValue)
 	{
-		return vertices.at(x).value;
-	}
-	else
-	{
-		return FLT_MAX;
-	}
-}
-
-bool Graph::setNodeValue(const std::string& x, const float nodeValue)
-{
-	bool success = false;
-	if (vertices.find(x) != vertices.end())
-	{
-		vertices.at(x).value = nodeValue;
-		success = true;
-	}
-	return success;
-}
-
-float Graph::getEdgeValue(const std::string& x, const std::string& y) 
-{
-	float egdeVal = FLT_MAX;
-	if (vertices.find(x) != vertices.end())
-	{
-		auto found = std::find_if(vertices.at(x).edges.begin(),
-			vertices.at(x).edges.end(),
-			[&](Edge edge) { return(edge.node == y); });
-		if (found != vertices.at(x).edges.end())
+		bool success = false;
+		auto found = find_if(nodeCost.begin(),
+			nodeCost.end(),
+			[&](std::pair<std::string, float> val) {return val.first == x; });
+		if (found != nodeCost.end())
 		{
-			egdeVal = found->value;
-		}
-	}
-	return egdeVal;
-}
-
-bool Graph::setEdgeValue(const std::string& x, const std::string& y, const float edgeValue)
-{
-	bool success = false;
-	if (vertices.find(x) != vertices.end())
-	{
-		auto found = std::find_if(vertices.at(x).edges.begin(),
-			vertices.at(x).edges.end(),
-			[&](Edge edge) { return(edge.node == y); });
-		if (found != vertices.at(x).edges.end())
-		{
-			found->value = edgeValue;
+			found->second = nodeValue;
 			success = true;
 		}
+		return success;
 	}
 
-	if (vertices.find(y) != vertices.end())
+	float Graph::getEdgeValue(const std::string& x, const std::string& y)
 	{
-		auto found = std::find_if(vertices.at(y).edges.begin(),
-			vertices.at(y).edges.end(),
-			[&](Edge edge) { return(edge.node == x); });
-		if (found != vertices.at(y).edges.end())
+		float edgeVal = FLT_MAX;
+		bool success = false;
+		auto found = std::find_if(edgeList.begin(),
+			edgeList.end(),
+			[&](Edge edge) {return (edge.start_node == x && edge.end_node == y); });
+		if (found != edgeList.end())
 		{
-			found->value = edgeValue;
-		    success &= true;
+			edgeVal = found->value;
 		}
+		return edgeVal;
 	}
 
-	return success;
+	bool Graph::setEdgeValue(const std::string& x, const std::string& y, const float edgeValue)
+	{
+		bool success = false;
+		success = del(x, y);
+		success &= add(x, y, edgeValue);
+		success &= add(y, x, edgeValue);
+		return success;
+	}
+
+	std::vector<Edge> Graph::getEdgeList()
+	{
+		return edgeList;
+	}
 }
