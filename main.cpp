de0c5644c860bc1ba3b5be68c43935e5b85f1b0e
Aparajith <s.aparajith@live.com>
2020-10-18 01:42:05 +0200

    update CmakeLists and test Kruskals implementation.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
 #include"include/kruskal_simulation.h"
+using KruskalSim::KruskalSimulation;
+
 int main()
 {
 	std::string fileName("SampleTestData_mst_data.txt");
 	KruskalSimulation sim(fileName);
 	sim.printGraph();
-
+	sim.runKruskal();
+	sim.printResult();
 	return 0;
 }
