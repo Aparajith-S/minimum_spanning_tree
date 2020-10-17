e9eba543eef06eae3d914078c2eea87f1457df3f
Aparajith <s.aparajith@live.com>
2020-10-18 01:40:50 +0200

    update simulation class to meet kruskal's algorithm requirement

--- include/kruskal_simulation.h
+++ include/kruskal_simulation.h
@@ -11,78 +11,35 @@
 ///////////////////////////////////////
 // Includes
 ///////////////////////////////////////
-#include"shortest_path.h"
+#include"minimum_spanning_tree.h"
 
-///////////////////////////////////////
-// Class definitions
-///////////////////////////////////////
-class KruskalSimulation
-{
-public:
-	KruskalSimulation(const uint32 numVertices,
-		const float ProbDensity,
-		const std::pair<float, float> distRange) :
-		G(numVertices),
-		SP(G),
-		density(ProbDensity),
-		numOfVertices(numVertices),
-		distanceRange(distRange)
+namespace KruskalSim {
+	///////////////////////////////////////
+	// Class definitions
+	///////////////////////////////////////
+	class KruskalSimulation
 	{
-		weight_generator();
-		random_graph_generator();
+	public:
+
+		KruskalSimulation(const std::string& fileName) :
+			G(fileName),
+			MST(G)
+		{};
+
+		///@brief runs the kruskals algorithm. it saves the result in member selectedEdges
+		/// @details use printResult to print the result of this computation 
+		void runKruskal();
+
+		///@brief prints the graph
+		void printGraph();
+
+		///@brief prints the edge lists forming the minimum spanning tree
+		void printResult();
+
+	private:
+
+		undirectedGraph::Graph G;
+		Kruskal::MinimumSpanningTree MST;
+		std::vector<undirectedGraph::Edge> selectedEdges;
 	};
-
-	KruskalSimulation(const std::string& fileName) :
-		G(fileName),
-		SP(G),
-		density(0),
-		distanceRange({ 0 , 0})
-	{
-		numOfVertices = G.V();
-	};
-
-	/// @brief run the shortest path algorithm workflow for all nodes and print paths, average, etc.
-	/// @return path average for all paths from the source.
-	float runKruskalAverage(std::string source);
-
-	///@brief prints the graph
-	void printGraph();
-
-	///@brief prints the shortest path link from source to destination if it exists
-	void print_path(const std::vector<std::string>& path, const std::string& srcNode);
-
-	/// @brief generates a weight given by range between all vertices randomly.
-	void weight_generator();
-	
-	/// @brief generates a random graph based on the density given
-	void random_graph_generator();
-
-	/// @brief re-draws the graph with a new density
-	/// @details Worst-case quadratic time complexity
-	void reGraph(float probDensity);
-
-private:
-
-	/// @brief Computes and returns the value based on probability density function on random values.
-	/// @details Treat this as an integral over the likelihood of a outcome in the normal distribution.
-	/// @return outcome [min,max] = [0.0 , 1.0]
-	float probability_density();
-
-	/// @brief A hash function used to hash a pair of any kind 
-	struct hash_pair {
-		template <class T1, class T2>
-		size_t operator()(const std::pair<T1, T2>& p) const
-		{
-			auto hash1 = std::hash<T1>{}(p.first);
-			auto hash2 = std::hash<T2>{}(p.second);
-			return hash1 ^ hash2;
-		}
-	};
-
-	Graph G;
-	ShortestPath SP;
-	std::unordered_map<std::pair<uint32, uint32>, float, hash_pair> edgeWeights;
-	float density;
-	uint32 numOfVertices;
-	std::pair<float, float> distanceRange;
-};
+}
--- source/kruskal_simulation.cpp
+++ source/kruskal_simulation.cpp
@@ -5,137 +5,49 @@
 /// @details	Main entry point for the entire project. Ostream usage is assumed to be allowed to print to console/file
 /// @history
 /// @see		ReadMe.md for more details. 
-/// @see		dijkstra_simulation.h for more details
+/// @see		kruskal_simulation.h for more details
 
 #include"kruskal_simulation.h"
 #include<iostream>
-
-float KruskalSimulation::runKruskalAverage(std::string source)
-{
-	int i = 0;
-	float averageCost = 0;
-	int numberFairTrials = G.V()-1;
-
-	while (i < G.V())
-	{
-		float cost = FLT_MAX;
-		std::string destination = name_generator(i++);
-		if (source != destination)
-		{
-			//prints the path using the shortest path obtained
-			print_path(SP.path(source, destination), source);
-			//gets the path size associated with the obtained shortest path
-			cost = SP.path_size(source, destination);
-			//print the path
-			std::cout << "Total cost is : " << cost << std::endl;
-			
-			if (cost == FLT_MAX)
-			{
-				numberFairTrials--;
-			}
-			else
-			{
-				averageCost += cost;
-			}
-		}
+using undirectedGraph::Edge;
+using undirectedGraph::name_generator;
+namespace KruskalSim {
+	void KruskalSimulation::runKruskal()
+	{ 
+		selectedEdges = MST.ComputeKruskalMST();
 	}
-	averageCost /= numberFairTrials;
-	std::cout << "Average Cost for 50 paths:" << averageCost << std::endl;
-	
-	return averageCost;
-}
 
-void KruskalSimulation::printGraph()
-{
-	std::cout << "Graph layout"<<std::endl;
-	std::cout << "Src Node" << "   " << "Dst Node" << "  Distance " << std::endl;
-	for (uint32 i = 0; i < G.V(); i++)
+	void KruskalSimulation::printGraph()
 	{
-		for (uint32 j = 0; j < G.V(); j++)
+		std::cout << "Graph layout" << std::endl;
+		std::cout << "Src Node" << "   " << "Dst Node" << "  Distance " << std::endl;
+		for (uint32 i = 0; i < G.V(); i++)
 		{
-			if (i < j)
+			for (uint32 j = 0; j < G.V(); j++)
 			{
-				std::string x = name_generator(i);
-				std::string y = name_generator(j);
-				if (G.adjacent(x, y))
+				if (i < j)
 				{
-					std::cout << '\t' << x << '\t' << y << '\t' << G.getEdgeValue(x, y) << "\t" << std::endl;
+					std::string x = name_generator(i);
+					std::string y = name_generator(j);
+					if (G.adjacent(x, y))
+					{
+						std::cout << '\t' << x << '\t' << y << '\t' << G.getEdgeValue(x, y) << "\t" << std::endl;
+					}
 				}
 			}
 		}
 	}
-}
 
-void KruskalSimulation::print_path(const std::vector<std::string>& path, const std::string& srcNode)
-{
-
-	if (path.at(path.size() - 1) != srcNode)
+	void KruskalSimulation::printResult()
 	{
-		std::cout << "node "<<path[0]<<" is isolated." << std::endl;
-	}
-	else
-	{
-		std::cout << "Path: Source =>";
-		for (auto iter = path.rbegin();
-			iter != path.rend();
-			iter++)
+		float cost = 0.0F;
+		std::cout << "Edges of minimum spanning tree : ";
+		for (auto& edge : selectedEdges) 
 		{
-			if (iter != path.rend() - 1)
-			{
-				std::cout << *iter << " --> ";
-			}
-			else
-			{
-				std::cout << *iter;
-			}
+			std::cout << std::endl<< "[" << edge.start_node << "-" << edge.end_node << "](" << edge.value << ") ";
+			cost += edge.value;
 		}
-
-		std::cout << "<= Destination" << std::endl;
+		std::cout << std::endl;
+		std::cout << "The cost for the minimum spanning tree is:"<<cost;
 	}
-}
-
-/// @brief generates a weight given by range between all vertices randomly.
-void KruskalSimulation::weight_generator()
-{
-	for (uint32 i = 0; i < numOfVertices - 1; i++)
-	{
-		for (uint32 j = 0; j < numOfVertices; j++)
-		{
-			//this is a undirected graph so remember we need only the upper triangle 
-			//of the vertex matrix in case of full connectivity. 
-			//Also it stores only the i->j connection weight as the converse j->i is redundant information.
-			if (i < j)
-			{
-				edgeWeights.insert({ {i,j}, float(rand() % (static_cast<uint32>(distanceRange.second - distanceRange.first) * 100) + distanceRange.first * 100) / 100.0F });
-			}
-		}
-	}
-}
-
-/// @brief generates a random graph based on the density given
-void KruskalSimulation::random_graph_generator()
-{
-	for (uint32 i = 0; i < numOfVertices - 1; i++)
-	{
-		for (uint32 j = 0; j < numOfVertices; j++)
-		{
-			if (i < j && probability_density() < density)
-			{
-				G.add(name_generator(i), name_generator(j), edgeWeights.at({ i, j }));
-			}
-		}
-	}
-}
-
-/// @brief re-draws the graph with a new density
-/// @details Worst-case quadratic time complexity
-void KruskalSimulation::reGraph(float probDensity)
-{
-	density = probDensity;
-	random_graph_generator();
-}
-
-float KruskalSimulation::probability_density()
-{
-	return float(rand() % 101) / 100.0F;
 }
